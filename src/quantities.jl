73d983957535ef053f5daf3b6af6ed644d87711b
Nick OBrien <nick4f42@gmail.com>
2022-03-05 20:46:01 -0600

    Add functions for lift and drag coefficients

--- src/quantities.jl
+++ src/quantities.jl
@@ -3,7 +3,24 @@ module Quantities
 using ..IBPM: IBState
 
 
-# TODO: Add functions for each quantity in IBState
+"""
+    lift_coef(state)
+"""
+function lift_coef end
+
+"""
+    drag_coef(state)
+"""
+function drag_coef end
+
+# TODO: Add functions for remaining quantities in IBState
+
+for (name, field) in (
+    (:lift_coef, :CL),
+    (:drag_coef, :CD),
+)
+    @eval $name(state::IBState) = deepcopy(state.$field)
+end
 
 
 end # module
