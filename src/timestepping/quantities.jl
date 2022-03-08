c2a3756df3f26a912c2d703018917a1889d63173
Nick OBrien <nick4f42@gmail.com>
2022-03-08 12:39:54 -0600

    Add remaining quantities to `Quantities` module

--- src/timestepping/quantities.jl
+++ src/timestepping/quantities.jl
@@ -4,20 +4,73 @@ using ..IBPM: IBState
 
 
 """
-    lift_coef(state)
+    vel_flux(state)
 """
-function lift_coef end
+function vel_flux end
+
+"""
+    vel_potential(state)
+"""
+function vel_potential end
+
+"""
+    circulation(state)
+"""
+function circulation end
+
+"""
+    streamfunction(state)
+"""
+function streamfunction end
+
+"""
+    surface_stress(state)
+"""
+function surface_stress end
+
+"""
+    body_impulse(state)
+"""
+function body_impulse end
 
 """
     drag_coef(state)
 """
 function drag_coef end
 
-# TODO: Add functions for remaining quantities in IBState
+"""
+    lift_coef(state)
+"""
+function lift_coef end
+
+"""
+    cfl(state)
+"""
+function cfl end
+
+"""
+    slip(state)
+"""
+function slip end
+
+"""
+    body_points(state)
+"""
+function body_points end
+
 
 for (name, field) in (
-    (:lift_coef, :CL),
+    (:vel_flux, :q),
+    (:vel_potential, :q0),
+    (:circulation, :Γ),
+    (:streamfunction, :ψ),
+    (:surface_stress, :fb),
+    (:body_impulse, :F̃b),
     (:drag_coef, :CD),
+    (:lift_coef, :CL),
+    (:cfl, :cfl),
+    (:slip, :slip),
+    (:body_points, :xb),
 )
     @eval $name(state::IBState) = deepcopy(state.$field)
 end
