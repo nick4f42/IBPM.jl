188942c269209dc9f01c9845dc48a6b06ab5f0a6
Nick OBrien <nick4f42@gmail.com>
2022-03-08 12:23:17 -0600

    Update example `cyl_100.jl` with new UI

--- examples/cyl_100.jl
+++ examples/cyl_100.jl
@@ -1,103 +1,44 @@
 include("../src/IBPM.jl")
+
+import Plots
+using Plots: plot, @animate, gif
+
 using .IBPM
-using FileIO #For saving data as a jld2 file
+using .IBPM.Quantities: lift_coef
 
-#Below, the Reynolds #, boundary of the finest domain, and body must be specified.
-#all other variables have default values and needn't be provided the user
+Δx = 0.02  # Grid step size
+boundary = ((-1.0, 3.0), (-2.0, 2.0))  # x and y boundaries of domain
+gridcount = 5  # Number of sub-domains
+grid = MultiGrid(Δx, boundary; mg=gridcount)
 
-#--necessary variables
-    boundary = (-1.0, 3.0, -2.0, 2.0) #left, right, bottom, and top of domain
-    Re = 100.0 #Reynolds #
-    # specify body as a vector of named tuples with keys type, lengthscale,
-    #center, motion.
-    #Type and lengthscale must be specified. The others have defaults associated
-    #with a stationary body centered at (x,y)=(0.0,0.0)
-    type = :cylinder #:cylinder, :plate are supported
-    lengthscale = 0.5 #key lengthscale. e.g., for cylinder is radius. Supports Float64
-    motion = :static #type of body motion. supports :static or a function of time
-                     #default is static
-    center = [0.0; 0.0] #body CoM is centered here. default: [0.0; 0.0]
-                         #displace in y to trigger asymmetry
-    body = [(type=type, lengthscale=lengthscale, motion=motion, center=center)]
-#--
+r = 0.5
+bodies = [ IBPM.make_cylinder(r, Δx, 0.0, 0.0) ]
 
-#--optional variables
-    freestream = (Ux=t->t^0.0,) #freestream conditions
-                                #can be provided as constants or
-                                #functions of time
-                                #(default: (Ux=0.0, Uy=0.0, inclination=0.0))
+Δt = 0.004  # Time step size
+T = 10.0  # Final time
+times = range(0, T, step=Δt)
 
-    T = 1.0 #final time to run to (default = 20.0*dt)
+# Δt can be autodetermined by supplying a tuple of initial and final times.
+# The default aims for a CFL of 0.1 with a 5x safety factor on max velocity.
+# times = (0.0, 1.0)
 
-    #simulation parameters (these are all optional)
-    Δx = 0.02 #default (==missing) gives a grid Re of 2
-    Δt = 0.004 #default (==missing) aims for a CFL of 0.1 with a
-              #fairly conservative safety factor on max vel
-    mg=5      #Number of sub-domains. Default is 5
-#--
+Re = 100.0  # Reynolds number
+freestream = t -> (1.0, 0.0)  # Freestream velocity as a function of time
 
-#--save_info (optional)
-    #gives the code information for a data structure to return
-    #user provides as a Named Tuple with three keys: save_fcns, save_times,
-    #   save_types
-    #default (if save_info is unspecified): the code returns necessary
-    #   information for a restart.
+# lift_coef returns the lift coefficient for each body, so take the first
+lift = StateData(state -> lift_coef(state)[1])
+# By default, the entire state is saved
+states = StateData(saveat=LinRange(0, T, 31))
 
-    #Even if save_info is provided, only the save_fcns key is necessary.
+problem = IBProblem(grid, bodies, times; Re, freestream)
+solve!([lift, states], problem)
 
-    #vector of functions that specify which data to save.
-    #default: save full state at the final time
-    #NOTE: if you want to plot the flowfield later, you have to save the full
-    #state as (t,state)-> state
-    svfc = [ (t,state)->state.CL; (t,state)->state  ]
+anim = @animate for (t, state) in zip(states.t, states)
+    print("t = ", t, '\r')
+    IBPM.plot_state(
+        problem, state, t, var=:omega,
+        xlims=(-4.0, 10.0), ylims=(-3.0, 3.0), clims=(-5.0, 5.0), clevs=40
+    )
+end
 
-    #corresponding vector of save instances (1 per function)
-    #each can be prescribed as scalar Float or array.
-    #default: save at the final time instance T for each save variable
-    #if any of the entries is either 0.0 or missing, that variable will be saved
-    #every timestep
-    svti = [ 0.0, T/30.0 ]
-    #types of each variable
-    #default: Any for each save variable
-    svty = [ Vector{Float64}; Any ]
-
-    #Store in Named Tuple
-    save_info = ( save_fcns = svfc, save_types = svty, save_times = svti )
-
-#--
-
-#--run the simulation based on user data
-    prob, data, runtime = IBPM_advance( Re, boundary, body, freestream,
-        Δx=Δx, Δt=Δt, T=T, save_info=save_info )
-
-        #Uncomment to save data as a JLD2 file (caution, could be large!)
-        # FileIO.save("examples/output.jld2",  "data", data, "prob", prob)
-#--
-
-
-#--after running, you can use the data struct to plot/analyze quantities.
-    using Plots
-
-    #Looking at timetraces of lift or drag is a good starting point. Lift was
-    #the first variable saved in our data struct so we can plot that as...
-    display(
-        plot( data[1].t, [data[1].val[j][1] for j in 1:length(data[1].t)],
-        legend=:false )
-        )
-
-    #Plotting the flowfield is trickier, so there's more built-in support for
-    #that. First off, we can plot a snapshot of the flowfield in terms of either
-    #vorticity (var=:omega, default), x and y velocity (var=:vel), or the
-    #streamfunction (var=:psi). Let's do that at the final time:
-    IBPM.plot_state( prob, data[2].val[end], data[2].t[end], var=:omega,
-        xlims=(-4.0, 10.0), ylims=(-3.0, 3.0), clims=(-5.0, 5.0), clevs=40)
-
-    #We can also use Julia's handy @animate macro to make a gif of the vorticity
-    # field from our save data:
-    anim = @animate for j = 1 : length(data[2].t)
-                IBPM.plot_state( prob, data[2].val[j], data[2].t[j], var=:omega,
-                xlims=(-4.0, 10.0), ylims=(-3.0, 3.0), clims=(-5.0, 5.0), clevs=40 )
-            end
-    #To save the animation, use gif():
-    gif(anim, "cyl_100.gif", fps=10)
-#--
+gif(anim, "cyl_100.gif", fps=10)
